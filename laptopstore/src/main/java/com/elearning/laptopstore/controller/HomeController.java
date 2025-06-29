a833f460c2a881554d2da4653ccb15b6ab6df590
HasiniHemathilaka <nimeshahemathilaka@gmail.com>
2025-06-30 00:54:03 +0530

    Adding a home page

---
+++ laptopstore/src/main/java/com/elearning/laptopstore/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.elearning.laptopstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Welcome to Laptop Store");
+        return "home";
+    }
+}
